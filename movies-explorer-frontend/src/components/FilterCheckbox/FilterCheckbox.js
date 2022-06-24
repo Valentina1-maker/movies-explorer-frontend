7e65336d17009d87d0d382ec11e527d72b8a3604
Valentina <valentina.kramoreva@yandex.ru>
2022-06-24 17:50:55 +0400

    fix chechbox

--- movies-explorer-frontend/src/components/FilterCheckbox/FilterCheckbox.js
+++ movies-explorer-frontend/src/components/FilterCheckbox/FilterCheckbox.js
@@ -4,13 +4,13 @@ function FilterCheckbox() {
   return (
     <form className="checkbox">
       <label className="checkbox__form-label" htmlFor="short-film">
-        <span className="checkbox__form-text">Короткометражки</span>
         <input
           className="checkbox__input checkbox__input_hidden"
           name="short-film"
           type="checkbox"
           id="short-film"/>
         <span className="checkbox__input checkbox__input_visible" />
+        <span className="checkbox__form-text">Короткометражки</span>
       </label>
     </form>
   );
