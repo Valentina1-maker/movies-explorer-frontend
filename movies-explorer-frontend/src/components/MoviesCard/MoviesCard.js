ff01a58366196139da871940392768ce3ac2c3d0
Valentina <valentina.kramoreva@yandex.ru>
2022-06-25 20:19:02 +0400

    добавила alt изменила тег в searchform

--- movies-explorer-frontend/src/components/MoviesCard/MoviesCard.js
+++ movies-explorer-frontend/src/components/MoviesCard/MoviesCard.js
@@ -4,7 +4,7 @@ import foto from "../../images/image-film.png"
 function MoviesCard() {
     return (
         <div className="card">
-            <img className="card__picture" alt="" src={foto} />
+            <img className="card__picture" alt="Здесь изображение фильма" src={foto} />
             <div className="card__description">
                 <h2 className="card__title">Бег - это свобода</h2>
                 <button className="card__like-button card__like-button_liked" />
--- movies-explorer-frontend/src/components/SearchForm/SearchForm.js
+++ movies-explorer-frontend/src/components/SearchForm/SearchForm.js
@@ -4,25 +4,24 @@ import FilterCheckbox from '../FilterCheckbox/FilterCheckbox'
 
 
 function SearchForm() {
-    return (
-        <section className="search">
-          <div className="search__container">
-            <form className="search__form">
-              <label htmlFor="search" className="search__form-label">
-                <input className="search__form-input"
-                  type="text"
-                  id="search"
-                  placeholder="Фильм"
-                  required/>
-                <input className="search__button"
-                type="submit"
-                value="Найти"/>
-              </label>
-            </form>
-            <FilterCheckbox/>
-          </div>
-        </section>
-      );
-    }
- 
+  return (
+    <section className="search">
+      <div className="search__container">
+        <form className="search__form">
+          <label htmlFor="search" className="search__form-label">
+            <input className="search__form-input"
+              type="text"
+              id="search"
+              placeholder="Фильм"
+              required />
+            <button className="search__button"
+              type="submit">Найти</button>
+          </label>
+        </form>
+        <FilterCheckbox />
+      </div>
+    </section>
+  );
+}
+
 export default SearchForm;
